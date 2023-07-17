487cd1db91f91d1c6f63cda57a72f14e5d1589e7
Campbell Vertesi <campbell@vertesi.com>
2023-07-17 12:33:33 +0200

    Add TestData

---
+++ TestData.js
@@ -0,0 +1,125 @@
+
+const fwdSubject = "Fwd: Test Subject";
+const fwdBody = `---------- Forwarded message ---------
+From: Mister Someone <someone@example.com>
+Date: Thu, Jun 1, 2023 at 8:47 AM
+Subject: Fwd: Buchungsbestätigung (Auftrag 9DDD99)
+To: Miss Someone Else <someone.else@example.com>
+
+
+
+
+Von meinem iPhone gesendet
+
+Anfang der weitergeleiteten Nachricht:
+
+*Von:* noreply@example.com
+*Datum:* 17. Mai 2023 um 22:36:20 MESZ
+*An:* someone@example.com
+*Betreff:* *Buchungsbestätigung (Auftrag 9DDD99)*
+*Antwort an:* noreply@example.com
+
+﻿
+
+[image: DB Bahn] <http://www.bahn.de/>
+*Buchungsbestätigung (Auftrag 9DDD99)*
+
+*Sehr geehrter Herr Someone,*
+
+vielen Dank für Ihren Fahrkartenkauf.
+
+*Ihre Buchungsdaten:*
+
+Auftragsnummer: 9DDD99
+Kundennummer: 9999999
+------------------------------
+
+   -
+   Falls Sie die App "DB Navigator" auf Ihrem Android Smartphone oder
+   iPhone installiert haben, können Sie Ihr Handy-Ticket über folgenden Link
+   herunterladen:
+
+   Auftrag laden für iOS DB Navigator
+   Auftrag laden für Android DB Navigator
+   <http://dbnavigator.bahn.de/loadorder?&on=9DDD99&name=Something>
+
+   Die App erhalten Sie über bahn.de/apps oder suchen Sie in den App-Stores
+   nach "DB Navigator".
+
+*Das sollten Sie wissen:*
+
+   -
+   Das Ticket gilt nur für Mister Someone persönlich (ggf. auch für Mitfahrer) in
+   Verbindung mit einem amtlichen Lichtbildausweis (z.B. Personalausweis oder
+   Reisepass) oder einer gültigen BahnCard.
+   -
+   Bei der Kontrolle wird der Zugbegleiter das Ticket sowie den amtlichen
+   Lichtbildausweis bzw Ihre BahnCard prüfen. Bitte halten Sie hierfür Ticket
+   und Ihren Personalausweis, Reisepass oder BahnCard griffbereit.
+
+Weitere Hinweise zur Nutzung Ihres Onlinetickets können Sie *hier*
+<http://www.bahn.de/beipackzettel_pk_de_DEU.shtml> einsehen und ausdrucken.
+------------------------------
+
+*Tickets erneut abrufen oder stornieren*
+
+Die gebuchten Leistungen können Sie bis zum Ende der Gültigkeit über
+www.bahn.de/auftragssuche oder die Buchungsrückschau (nur für eingeloggte
+Kunden) erneut ausdrucken.
+
+Falls sich Ihre Reiseplanung ändert, können Sie die gebuchten Leistungen im
+Rahmen der tariflichen Zulässigkeit über www.bahn.de/erstattung stornieren.
+
+Handy-Tickets sind auch mobil über www.m.bahn.de stornierbar.
+------------------------------
+
+*Tipps und Tricks:*
+
+   -
+   Aktuelle Information zu Ihrer Verbindung, z.B. Pünktlichkeit,
+   Anschlüsse, Bahnhofsinformation und Karten (nur bei Buchungen, die über die
+   Reiseauskunft erfolgten) finden Sie im
+   Reiseplan <http://www.bahn.de/reiseplan>
+   Aktuelle Bahnhofstafeln <http://www.bahn.de/zug_puenktlich>
+
+------------------------------
+
+Wir wünschen Ihnen eine gute Reise.
+
+Mit freundlichen Grüßen
+
+[image: Ihr Team von www.bahn.de]
+
+*Hilfe und Kontakt*
+
+<http://www.bahn.de/auftragssuche>
+<http://www.bahn.de/meinebahn>
+<https://chatbot-bahn.de>
+<https://inside.bahn.de/services-bahn-social-media>
+<http://www.bahn.de/bahncard>
+<http://www.bahn.de/kontakt>
+<http://www.bahn.de/mobile>
+*Meine Buchungen* *Kundenkonto* *Chatbot* *Social Media* *BahnCard*
+*Kontakt* *Mobile*
+
+*Impressum*
+
+DB Fernverkehr AG
+Europa-Allee 78 - 84
+60486 Frankfurt am Main
+
+Registergericht B Frankfurt am Main
+HRB 83 173
+Ust-IdNr.: DE 260656754
+
+Die DB Fernverkehr AG wird vertreten durch den Vorstand: Dr. Michael
+Peterson (Vorsitzender), Stefanie Berk (Marketing und Vertrieb), Wilken
+Bormann (Finanzen), Martin Jende (Personal), Anja Schöllmann (Produktion).
+© Deutsche Bahn AG
+
+
+
+-- 
+Miss Someone Else
++49 152 11111111
+example.com`;
